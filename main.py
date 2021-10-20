3d70c8bea327f696e0cb8afe6c8efe8d1bcee545
m <m@m.m>
2021-10-19 22:49:15 -0300

    commit inicial

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 from src.rgbot import Bot
+from decouple import config
+
+def main():
+    # Lectura de variables de conexión
+    CONSUMER_KEY = config('CONSUMER_KEY')
+    CONSUMER_SECRET = config('CONSUMER_SECRET')
+    ACCESS_KEY = config('ACCESS_KEY')
+    ACCESS_SECRET = config('ACCESS_SECRET')
+    
+    # Inicializamos el bot
+    bot = Bot(
+        CONSUMER_KEY,
+        CONSUMER_SECRET,
+        ACCESS_KEY,
+        ACCESS_SECRET
+    )
+
+    # Inicializamos la conexión
+    bot.connect()
+
+    # Hacemos la demo
+    bot.demo()
+
 
 if __name__ == '__main__':
-    Bot().prueba()
+    main()
--- src/rgbot.py
+++ src/rgbot.py
@@ -1,6 +1,24 @@
-class Bot:
-    def __init__(self):
-        pass
+import tweepy
 
-    def prueba(self):
-        print('hola...')
+class Bot:
+    def __init__(self, consumer_key, consumer_secret, access_key, access_secret):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_key = access_key
+        self.access_secret = access_secret
+    
+    def connect(self):
+        self.auth = tweepy.OAuthHandler(
+            self.consumer_key,
+            self.consumer_secret)
+
+        self.auth.set_access_token(
+            self.access_key,
+            self.access_secret)
+
+    def demo(self):
+        api = tweepy.API(self.auth)
+        
+        public_tweets = api.home_timeline()
+        for tweet in public_tweets:
+            print(tweet.text)
